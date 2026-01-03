aff0a539906cd864adbc5491d88d05e5d1a6e93e
Thiện Lê <lethien999@users.noreply.github.com>
2026-01-04 00:19:50 +0700

    fix: Add DatabaseConfig.ci.cs and copy during CI build

---
+++ DAO/DatabaseConfig.ci.cs
@@ -0,0 +1,7 @@
+namespace WinFormsFashionShop.Data
+{
+    public static class DatabaseConfig
+    {
+        public static string ConnectionString { get; set; } = "Data Source=localhost;Initial Catalog=WinFormsFashionShopDb;Integrated Security=True;Trust Server Certificate=True";
+    }
+}
